c47f1654eb0b9db4e5572c3301faab0de4089a1f
Shumpei Nagata <nagata.syunpei@gmail.com>
2024-10-13 16:56:20 +0900

    Adding side effectsのManaging a timerまで

--- Study-TCA/Study-TCA/CounterFeature.swift
+++ Study-TCA/Study-TCA/CounterFeature.swift
@@ -15,6 +15,7 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
@@ -22,6 +23,12 @@ struct CounterFeature {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+    
+    enum CancelID {
+        case timer
     }
     
     var body: some ReducerOf<Self> {
@@ -51,6 +58,25 @@ struct CounterFeature {
                 state.count += 1
                 state.fact = nil
                 return .none
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
@@ -83,6 +109,14 @@ struct CounterView: View {
         .background(Color.black.opacity(0.1))
         .cornerRadius(10)
       }
+        
+        Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+              }
+              .font(.largeTitle)
+              .padding()
+              .background(Color.black.opacity(0.1))
+              .cornerRadius(10)
         
         Button("Fact") {
                 store.send(.factButtonTapped)
